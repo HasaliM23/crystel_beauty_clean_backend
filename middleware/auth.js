0342bc69205c6a42f64f6f83be96c73409e2b075
HasaliM23 <hasali2000pm@gmail.com>
2025-05-06 00:03:39 -0700

    jwt key hiding 🔑

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,14 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+
 export default function verifyJWT (req, res, next){
   const header = req.header("Authorization");
   /// oyage prashne thibbe Authorization name eka waradi ekai  frontend eken eken Authorization gnn be ethakota
   if (header != null) {
     const token = header.replace("Bearer ", "");
-    jwt.verify(token, "random456", (err, decoded) => {
+    jwt.verify(token,process.env.JWT_KEY, (err, decoded) => {
       console.log(decoded);
       if (decoded != null) {
         req.user = decoded;
